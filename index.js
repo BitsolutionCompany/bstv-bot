8102fc2326bcdcf59e7aee0ed87a61d294a06566
BitsolutionCompany <166075575+BitsolutionCompany@users.noreply.github.com>
2026-01-02 19:44:23 -0300

    Commit de teste

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import Whatsapp from "whatsapp-web.js";
 import qrcode from "qrcode-terminal";
-import http from 'http';
 
 const { Client, LocalAuth } = Whatsapp;
 
+
 const client = new Client({
-  authStrategy: new LocalAuth({ dataPath: './session' }),
-  puppeteer: { 
-    headless: true,
+    authStrategy: new LocalAuth(),
+    puppeteer: { headless: true },
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
@@ -18,40 +17,21 @@ const client = new Client({
       '--single-process',
       '--disable-gpu'
     ]
-  },
 });
 
 client.on("qr", (qr) => {
-  console.log("=================================================");
-  console.log("📷 QR Code recebido. ESCANEIE O LOG RAPIDAMENTE:");
-  qrcode.generate(qr, { small: true });
-  console.log("=================================================");
+    console.log("QR RECEIVED", qr);
+    qrcode.generate(qr, { small: true });
 });
 
 client.on("ready", () => {
-    console.log("✅ Cliente do WhatsApp está pronto!");
-})
+    console.log("Client is ready!");
+});
 
 client.on("message", async (message) => {
-    if (message.body === "!ping"){
-        message.reply("!pong")
+    if (message.body.toLowerCase() === "ping") {
+        message.reply("pong");
     }
-})
-
-client.on("auth_failure", (msg) => console.error("❌ Falha na Autenticação:", msg));
-
-client.on("disconnected", (reason) => {
-  console.error("🚫 Cliente desconectado. Tentando reconectar...", reason);
-  process.exit(1);
 });
 
 client.initialize();
-
-const PORT = process.env.PORT || 8080;
-
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('WhatsApp Bot Worker is running.\n');
-}).listen(PORT, () => {
-  console.log(`Worker Webhook placeholder rodando na porta: ${PORT}`);
-});
